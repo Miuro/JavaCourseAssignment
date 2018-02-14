592a122d9fed6962e0787fa6282d8d079be15604
josirkka <josirkka@ac44247f-3231-4262-bf68-bb745410ccdf>
2018-02-14 21:26:14 +0000

    Suuri branchaus operaatio.

---
+++ WIP/src/FXvarasto/TulostusController.java
@@ -0,0 +1,10 @@
+package FXvarasto;
+
+/**
+ * @author Omistaja
+ * @version 30.1.2018
+ *
+ */
+public class TulostusController {
+      // TODO
+}
---
+++ WIP/src/FXvarasto/VarastoController.java
@@ -0,0 +1,10 @@
+package FXvarasto;
+
+/**
+ * @author JSirkka, Miro Korhonen
+ * @version 30.1.2018
+ *
+ */
+public class VarastoController {
+      // TODO
+}
